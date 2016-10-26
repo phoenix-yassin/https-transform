281dae58700eb14760cb330cad576f131f9e9197
易云辉 <yiyunhui@cyou-inc.com>
2016-10-26 11:05:07 +0800

    fix:修改request的方式

--- test_code/httpsServerPipe.js
+++ test_code/httpsServerPipe.js
@@ -27,79 +27,82 @@ var server = https.createServer(options, function (req, res) {
   console.log(JSON.stringify(urlObj));
   console.log(req.headers.host + '<==>' + newUrl);
 
-  var ext = path.extname(urlObj.path);
-  ext = ext ? ext.slice(1) : 'unknown';
+  var ext = path.extname(urlObj.path),
+    ext = ext ? ext.slice(1) : 'unknown';
   var contentType = mime[ext] || "text/plain";
   var acceptEncoding = req.headers['accept-encoding'] || "";
   var retType = getReqFileType(req.url);
+  var decompressed;
+  var responseEncoding;
   console.log('type:'+ retType );
- //var matched = ext.match(config.Compress.match);
-  /*var cReq = http.request(newUrl, function (cRes) {
-    var body = [];
-    cRes.on('data', function (chunk) {
-      body.push(chunk);
-      console.log("chunk: " + chunk.toString());
-    });
-    cRes.on('end', function () {
-      body = Buffer.concat(body);
-      console.log('end!');
-      if (cRes.headers['content-encoding'] === 'gzip') {
-        res.send(body);
-          zlib.gunzip(body, function (err, data) {
-            console.log(body.toString());
-          });
-      } else {
-        console.log(body.toString());
-      }
-    });
-  });
-  cq.end();*/
 
-    var oldRes = request(newUrl, function(error, response, body) {
-    //console.log('the decoded data is: ' + response.pipe(gunzipStream).toString());
-    //body.pipe(gunzipStream).pipe();
-    console.log('origin response header0:' + JSON.stringify(response.headers) );
-  });
-    var decompressed;
-  if (acceptEncoding.match(/\bgzip\b/)) {
-    if(retType === 'js' || retType === 'css' || retType === 'html'){
-<<<<<<< HEAD
-      req.pipe(oldRes);
-      if(retType === 'css'){
 
-      }
-      oldRes.pipe(zlib.createGunzip())
-        .pipe(replaceStream(/http:\/\//g, 'https://'))
-=======
-   console.log('origin response header:' + JSON.stringify(oldRes.headers) );  
-    //req.pipe(oldRes);
-    // oldRes.setHeader('content-type','application/javascript;charset=utf-8');
-    console.log('response header:' + JSON.stringify(oldRes.headers) );
-    decompressed =  oldRes.pipe(zlib.createGunzip());
-    decompressed.on('error', function(e){
-	console.log('gzib error:' + e);
-})
-    decompressed.pipe(replaceStream(/http:\/\//g, 'https://'))
-   // oldRes.pipe(replaceStream(/http:\/\//g, 'https://'))
->>>>>>> 1eba6c0efa093ac42edd16aadd77ac7f55babd24
-        /*.pipe(response({ compress: req }))*/
-        .pipe(res);
+  var oldReq = request(newUrl);
+  oldReq.on('reponse', function(oldRes) {
+    if (oldRes.statusCode !== 200) throw new Error('Status not 200');
+    responseEncoding = oldRes.headers['content-encoding'] || '';
+    if (responseEncoding.match(/\bgzip\b/)) {
+      if(retType === 'js' || retType === 'css' || retType === 'html'){
+        console.log('origin response header:' + JSON.stringify(oldRes.headers) );
+        //req.pipe(oldRes);
+        // oldRes.setHeader('content-type','application/javascript;charset=utf-8');
+        console.log('response header:' + JSON.stringify(oldRes.headers) );
+        decompressed =  oldRes.pipe(zlib.createGunzip());
+        decompressed.on('error', function(e){
+          console.log('gzib error:' + e);
+        })
+        decompressed.pipe(replaceStream(/http:\/\//g, 'https://'))
+       // oldRes.pipe(replaceStream(/http:\/\//g, 'https://'))
+            /*.pipe(response({ compress: req }))*/
+            .pipe(res);
       }else{
         req.pipe(oldRes);
         oldRes.pipe(res);
       }
+    } else if (responseEncoding.match(/\bdeflate\b/)) {
+      req.pipe(oldRes);
+      oldRes.pipe(zlib.createDeflate()).pipe(res);
+    } else {
+      req.pipe(oldRes);
+      oldRes.pipe(res);
+    }
+
+  });
+
+
+/*  var oldRes = request(newUrl, function(error, response, body) {
+    console.log('origin response header0:' + JSON.stringify(response.headers) );
+  });
+  if (acceptEncoding.match(/\bgzip\b/)) {
+    if(retType === 'js' || retType === 'css' || retType === 'html'){
+      console.log('origin response header:' + JSON.stringify(oldRes.headers) );
+      //req.pipe(oldRes);
+      // oldRes.setHeader('content-type','application/javascript;charset=utf-8');
+      console.log('response header:' + JSON.stringify(oldRes.headers) );
+      decompressed =  oldRes.pipe(zlib.createGunzip());
+      decompressed.on('error', function(e){
+        console.log('gzib error:' + e);
+      })
+      decompressed.pipe(replaceStream(/http:\/\//g, 'https://'))
+     // oldRes.pipe(replaceStream(/http:\/\//g, 'https://'))
+          //.pipe(response({ compress: req }))
+          .pipe(res);
+    }else{
+      req.pipe(oldRes);
+      oldRes.pipe(res);
+    }
   } else if (acceptEncoding.match(/\bdeflate\b/)) {
     req.pipe(oldRes);
     oldRes.pipe(zlib.createDeflate()).pipe(res);
   } else {
     req.pipe(oldRes);
     oldRes.pipe(res);
-  }
+  }*/
 
 });
 
-server.listen(443, '0.0.0.0');
 //server;
+server.listen(443, '0.0.0.0');
 server.on('error', function(e) {
   console.log(e);
 });
